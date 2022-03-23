f5e4aa6900cdc2de554802a661e8419abff287b6
OliveSerg <oliveserg@gmail.com>
2022-03-23 13:58:15 -0400

    Add sample data

--- app/src/main/java/com/example/comp4200/ProfileActivity.java
+++ app/src/main/java/com/example/comp4200/ProfileActivity.java
@@ -3,15 +3,50 @@ package com.example.comp4200;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.time.LocalDateTime;
+
+
 public class ProfileActivity extends AppCompatActivity {
+    //TODO: User model to import data from
+    private class User {
+        Integer user_id = 1337;
+        String username = "Test";
+        String handle = "test";
+        String first_name = "Tester";
+        String last_name = "Example";
+        String email = "test@example.com";
+        String image_id = "someimage.png";
+        String description = "This is a test profile";
+        LocalDateTime created = LocalDateTime.now();
+    }
     TextView name;
     TextView description;
+    ImageView imageView;
+    TextView handle;
+    TextView date_created;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        name = findViewById(R.id.user_name);
+        description = findViewById(R.id.user_description);
+        imageView = findViewById(R.id.user_image);
+        handle = findViewById(R.id.user_handle);
+        date_created = findViewById(R.id.user_created);
+
+        //TODO: Populate Text with real user data
+        User user = new User();
+        name.setText(user.first_name + " " + user.last_name);
+        description.setText(user.description);
+        handle.setText(user.handle);
+        date_created.setText(user.created.getMonth() + " " + user.created.getYear());
+//        imageView.setImageBitmap(bm); //TODO: Need to know how image is saved
+
+        //TODO: Get fragments of tweets and other data filtered by user
     }
 }
