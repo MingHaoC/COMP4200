5dafd2f008f8c1ac7e7fbb6a295fbd255b47c9d3
nehpethP <sarkis8@uwindsor.ca>
2022-03-25 13:22:01 -0400

    added login and compose tweet activities
    still need to link up to database to provide appropriate error messages for login activity and posting tweets to db. also need to link login activity to register activity

---
+++ app/src/main/java/com/example/comp4200/ComposeTweetActivity.java
@@ -0,0 +1,57 @@
+package com.example.comp4200;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComposeTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compose_tweet);
+
+        //change this button to a FAB later
+        Button tweetButton = findViewById(R.id.send_tweet);
+
+        /*
+         * grabs the text from the editText box on the page, when they click the tweet button it
+         * provides an error message if there are any, and sends the user back to the main page/timeline
+         * if there are no errors and they provided a valid tweet.
+         * @param view
+         */
+        tweetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText composeTweet = findViewById(R.id.composeTweet);
+                String tweetText = composeTweet.getText().toString();
+
+                TextView errorText = findViewById(R.id.tweet_errorMessage);
+
+                String error;
+
+                if(tweetText.isEmpty()){
+                    error = "Tweet cannot be empty";
+
+                }else if(tweetText.length() > 280){
+                    error = "Max tweet length is 280 characters.";
+                }else{
+                    error = "";
+                    //send tweet to db
+                    //then send user back to home page/timeline
+                    startActivity(new Intent(view.getContext(), MainActivity.class));
+                }
+
+                errorText.setText(error);
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/comp4200/MainActivity.java
+++ app/src/main/java/com/example/comp4200/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.comp4200;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toLoginButton = findViewById(R.id.login_button_main);
+
+        toLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), LoginActivity.class));
+            }
+        });
+
+        Button toComposeTweet = findViewById(R.id.tweet_button_main);
+
+        toComposeTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), ComposeTweetActivity.class));
+            }
+        });
     }
+
 }
