fc18bb319e8b8f20d9e266d15f06ca8af90fa412
nehpethP <sarkis8@uwindsor.ca>
2022-03-31 12:53:26 -0400

    activity and recyclerAdapter implemented
    Created and added a TweetRecyclerAdapter to the TimelineActivity, also created a Tweet object

---
+++ app/src/main/java/com/example/comp4200/Tweet.java
@@ -0,0 +1,34 @@
+package com.example.comp4200;
+
+public class Tweet {
+
+    String content;
+    String display_name;
+    String date;
+    int userID;
+
+    public Tweet(int userID, String content){
+
+        this.userID = userID;
+        this.content = content;
+
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public int getUserId(){
+        return this.userID;
+    }
+
+    public String getDisplay_name(){
+        return this.display_name;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/comp4200/TweetRecyclerAdapter.java
@@ -0,0 +1,84 @@
+package com.example.comp4200;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TweetRecyclerAdapter extends RecyclerView.Adapter<TweetRecyclerAdapter.ItemViewHolder> {
+
+    Context context;
+    ArrayList<Tweet> tweets;
+
+
+    public  TweetRecyclerAdapter(Context context, ArrayList<Tweet> tweets){
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tweet, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position){
+
+        String tweetContent = tweets.get(position).getContent();
+        String username = "" + tweets.get(position).getUserId();
+
+        holder.tweetContent.setText(tweetContent);
+        holder.tweetUser.setText(username);
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return tweets.size();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tweetUser;
+        TextView tweetContent;
+
+        CardView cardView;
+
+        ImageButton likes;
+        ImageButton replies;
+
+        ImageView tweetImage;
+        ImageView profileImage;
+
+        public ItemViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            tweetUser = itemView.findViewById(R.id.tvPostUsername);
+            tweetContent = itemView.findViewById(R.id.tvPostDesc);
+
+            cardView = itemView.findViewById(R.id.task);
+
+            likes = itemView.findViewById(R.id.ibComment2);
+            replies = itemView.findViewById(R.id.ibComment);
+
+            tweetImage = itemView.findViewById(R.id.imageView2);
+            profileImage = itemView.findViewById(R.id.ivPostPicture);
+
+        }
+
+    }
+
+}
