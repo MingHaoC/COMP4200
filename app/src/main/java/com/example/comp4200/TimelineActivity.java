c58e378225764bd4a053fca1c7add73ad0d59e10
Ming Hao Chen <chen1fl@uwindsor.ca>
2022-04-07 19:26:13 -0400

    [#11] User can now see their tweets on the TimelineActivity, needs to add the follower tweets

--- app/src/main/java/com/example/comp4200/TimelineActivity.java
+++ app/src/main/java/com/example/comp4200/TimelineActivity.java
@@ -1,85 +1,62 @@
 package com.example.comp4200;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.example.comp4200.DAO.UserDao;
 import com.example.comp4200.model.Tweet;
-import com.example.comp4200.model.User;
-import com.example.comp4200.service.AuthenticationService;
+import com.example.comp4200.service.TweetService;
 import com.example.comp4200.service.UserService;
-import com.example.comp4200.service.impl.AuthenticationServiceImpl;
+import com.example.comp4200.service.impl.TweetServiceImpl;
 import com.example.comp4200.service.impl.UserServiceImpl;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class TimelineActivity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-    TweetRecyclerAdapter adapter;
-    ArrayList<Tweet> tweets = new ArrayList<>();
-    ImageView profileImage;
-
-    FloatingActionButton composeTweetFAB;
-
-    FirebaseAuth firebaseAuth;
-    UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        recyclerView = findViewById(R.id.recyclerViewTweets);
-        profileImage = findViewById(R.id.imageView);
-        profileImage.setOnClickListener(view -> startActivity(new Intent(view.getContext(), ProfileActivity.class)));
-      
-        adapter = new TweetRecyclerAdapter(TimelineActivity.this, tweets);
-        recyclerView.setLayoutManager(new LinearLayoutManager(TimelineActivity.this));
-        recyclerView.setAdapter(adapter);
-
-        composeTweetFAB = findViewById(R.id.composeTweetFAB);
-        composeTweetFAB.setOnClickListener(view -> startActivity(new Intent(view.getContext(), ComposeTweetActivity.class)));
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        // Check if user is logged in
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if (firebaseUser == null) {
             startActivity(new Intent(this, LoginActivity.class));
             return;
         }
 
         // save the current logged in user info to the SharedPreferences
-        userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         SharedPreferences sp = getSharedPreferences("user", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         userService.getUser(firebaseUser.getUid(), editor);
 
-    }
+        // Recycler view and adapter
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        TweetRecyclerAdapter adapter = new TweetRecyclerAdapter(TimelineActivity.this, tweets);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewTweets);
+        recyclerView.setLayoutManager(new LinearLayoutManager(TimelineActivity.this));
+        recyclerView.setAdapter(adapter);
 
+        // Get tweet for user
+        TweetService tweetService = new TweetServiceImpl();
+        tweetService.getTweets(this, firebaseUser.getUid(), tweets, adapter);
+
+        // Post tweet button
+        FloatingActionButton composeTweetFAB = findViewById(R.id.composeTweetFAB);
+        composeTweetFAB.setOnClickListener(view -> startActivity(new Intent(view.getContext(), ComposeTweetActivity.class)));
+
+        // Add listener for the image to go to profile when click
+        ImageView profileImage = findViewById(R.id.imageView);
+        profileImage.setOnClickListener(view -> startActivity(new Intent(view.getContext(), ProfileActivity.class)));
+    }
 }
