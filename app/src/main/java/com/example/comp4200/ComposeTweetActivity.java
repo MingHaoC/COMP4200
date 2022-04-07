4c60c7f73f8df5f8d3b64b41c0b78215fd6acdb8
Ming Hao Chen <chen1fl@uwindsor.ca>
2022-04-07 15:55:06 -0400

    [#11] In the timeline page, if the user is already logged in we save the user information to the sharepreference under user, private_mode. [#12] User can now post tweets and it correctly saves it to the database. [#6] Tweet structure updated

--- app/src/main/java/com/example/comp4200/ComposeTweetActivity.java
+++ app/src/main/java/com/example/comp4200/ComposeTweetActivity.java
@@ -1,6 +1,7 @@
 package com.example.comp4200;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.comp4200.model.User;
+import com.example.comp4200.service.TweetService;
+import com.example.comp4200.service.impl.TweetServiceImpl;
+
 public class ComposeTweetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compose_tweet);
+
+        TweetService tweetService = new TweetServiceImpl();
+
 
         //change this button to a FAB later
         Button tweetButton = findViewById(R.id.compose_buttonSubmit);
@@ -26,27 +34,23 @@ public class ComposeTweetActivity extends AppCompatActivity {
          * if there are no errors and they provided a valid tweet.
          * @param view
          */
-        tweetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        tweetButton.setOnClickListener(view -> {
 
-                EditText composeTweet = findViewById(R.id.compose_etextComposition);
-                String tweetText = composeTweet.getText().toString();
+            EditText composeTweet = findViewById(R.id.compose_etextComposition);
+            String tweetText = composeTweet.getText().toString();
 
-                if(tweetText.isEmpty()){
-                    Toast.makeText(view.getContext(), "Tweet cannot be empty", Toast.LENGTH_LONG).show();
-
-                }else if(tweetText.length() > 280){
-                    Toast.makeText(view.getContext(), "Max tweet length is 280 characters.", Toast.LENGTH_LONG).show();
-                }else{
-                    //TODO connect to DB
-                    //send tweet to db
-                    //then send user back to home page/timeline
-                    startActivity(new Intent(view.getContext(), TimelineActivity.class));
-                }
-
-
+            if (tweetText.isEmpty())
+                Toast.makeText(view.getContext(), "Tweet cannot be empty", Toast.LENGTH_LONG).show();
+            else if (tweetText.length() > 280)
+                Toast.makeText(view.getContext(), "Max tweet length is 280 characters.", Toast.LENGTH_LONG).show();
+            else {
+                SharedPreferences sp = getSharedPreferences("user", MODE_PRIVATE);
+                User user = new User(sp.getString("id", ""), sp.getString("displayName", ""));
+                tweetService.PostTweet(ComposeTweetActivity.this, tweetText, user);
+                startActivity(new Intent(view.getContext(), TimelineActivity.class));
             }
+
+
         });
 
     }
